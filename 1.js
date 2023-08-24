831dec0e2908d3c430920f5afdfa2a47c35afe1a
Igor Stolbtsov <igorstg91@gmail.com>
2023-08-24 23:11:24 +0300

    1111

---
+++ 1.js
@@ -0,0 +1,44 @@
+//letnewLet1 = 'ourValue'
+//Symbol
+//let symbolOne = Symbol()
+
+//String
+
+let stringOneBuKuLu = 'ORIGINAL TEXT'
+//let stringOneTwo = "jdjdjdjd"
+//let stringPhrase = `let says ${stringOneBuKuLu}`
+
+console.log( typeof stringOneBuKuLu)
+
+let numberOne = 1
+console.log( stringOneBuKuLu / 0 )
+
+//Boolean
+let BoolOne = true
+let BoolTwo = false
+
+let nullOne = null
+console.log(nullOne)
+
+//undefined
+let undOne
+let undTwo = undefined
+console.log(undOne)
+
+
+
+var stringOneVar = 2323
+var ttt = 'ofd,jtts'
+
+let stringOneOne = 39993
+let string0000 = 'dflld' 
+
+const oneConstMy = 'igor'
+let my
+
+typeof 'str'
+console.log( typeof 'str' )
+
+let myVariable = 'value'
+typeof 'myVariable'
+console.log( typeof 'myVariable' )
