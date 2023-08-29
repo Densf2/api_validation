eb184d04787db0785dcedb19dca01aed3f768508
Igor Stolbtsov <igorstg91@gmail.com>
2023-08-29 20:58:08 +0300

    ddff

---
+++ new_loops.js
@@ -0,0 +1,27 @@
+let num = 11
+
+//while (num <= 7) {
+   // console.log(num)
+   // num++
+//}
+
+
+//do white
+//do {
+//num++
+//console.log(num)
+//} while (num <= 7)
+
+let sumOfLoop = 0;
+
+// for (let i = 0; i < 7; i++) {
+//     console.log(i)
+// }
+
+for (let i = 0; i <= num; i+=2) {
+console.log(i)
+if (i == 6) break;
+sumOfLoop += i
+}
+
+console.log(sumOfLoop)
